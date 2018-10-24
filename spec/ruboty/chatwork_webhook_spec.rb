311328fe0f64389c4df0ef92a5207da91ca098f4
honeniq <honeniq@gmail.com>
2018-10-25 00:16:00 +0900

    Remove broken test

--- spec/ruboty/chatwork_webhook_spec.rb
+++ spec/ruboty/chatwork_webhook_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Ruboty::ChatworkWebhook do
   it "has a version number" do
     expect(Ruboty::ChatworkWebhook::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
